137151e008e72dc8f08b55480253014d60f1d8e0
josepasu22 <josevictorpacaysuchite221297@gmail.com>
2026-03-15 17:30:09 -0600

    Corrige DB_PORT como número

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const pool = mysqlPromise.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   ssl: { rejectUnauthorized: false }
 });
 
